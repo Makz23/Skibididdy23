981e0788058aae57fedb4a7ed037e3daa32ab8de
Makz23 <146381649+Makz23@users.noreply.github.com>
2024-11-08 00:54:00 +0100

    Add files via upload

---
+++ L4 - SCSS — FINAL/Scripts/elements.js
@@ -0,0 +1,10 @@
+let isOn = false;
+
+document.getElementById("toggle").addEventListener("click", () => {
+    if (isOn) {
+        console.log("OFF!");
+    } else {
+        console.log("ON!");
+    }
+    isOn = !isOn; 
+});
